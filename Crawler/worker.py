9f7d5257fd158a8604c45e131f0253da9ea624ca
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-06 18:16:12 +0200

    init structure of thread crawler

---
+++ Crawler/worker.py
@@ -0,0 +1,15 @@
+import threading
+
+
+class CrawlerThread(threading.Thread):
+    """Class of a thread to crawl the web"""
+
+    def __init__(self, thread_id, name):
+        """Construct a new crawling thread"""
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.name = name
+
+    def run(self):
+        """The starting point of a thread"""
+        pass
