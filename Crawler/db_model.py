e29b245aef16da048589a2c84eaf78f462d292b8
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-07 17:03:46 +0200

    new database structure

--- Crawler/db_model.py
+++ Crawler/db_model.py
@@ -12,14 +12,20 @@ class BaseModel(Model):
 class Crawled(BaseModel):
     url = CharField(unique=True)
     content = TextField()
-    refresh = IntegerField()
     indexed = BooleanField()
+    visited = DateTimeField()
 
 
 class ToCrawl(BaseModel):
-    url = CharField()
+    url = TextField()
+    dns = CharField()
+    value = DoubleField()
+
+
+class Hasher(BaseModel):
+    hash = CharField(max_length=21, unique=True, index=True, null=False)
 
 
 def create_data_base():
     db.connect()
-    db.create_tables([Crawled, ToCrawl], safe=True)
+    db.create_tables([Crawled, ToCrawl, Hasher], safe=True)
---
+++ Crawler/storage.py
@@ -0,0 +1,10 @@
+"""File to handle all database transactions"""
+
+from peewee import *
+
+
+class Storage:
+    """This class handles database transactions and contain the temporary lists"""
+
+    def __init__(self):
+        pass
