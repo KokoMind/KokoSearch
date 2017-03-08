5fb05d26e2d22275d25f860cb15d1a7ee80d9f2e
Mostafa Gamal <mgo_oc@hotmail.com>
2017-03-08 22:04:14 +0200

    ...

--- Crawler/frontier.py
+++ Crawler/frontier.py
@@ -1,10 +1,12 @@
 from Crawler.prioritised_queue import PrioritisedQueue
+from Crawler.storage import Storage
 
 
 class Frontier:
     """Class which will contain ToCrawl pages"""
 
     def __init__(self, num_threads):
+        self.num_threads = num_threads
         self.to_serve = []
         self.queues = [PrioritisedQueue] * num_threads
         self.attended_websites = [list] * num_threads
@@ -19,7 +21,14 @@ class Frontier:
         return self.queues[thread_id].pop()[1]
 
     def save_to_crawl(self):
-        pass
+        arg = []
+        for i in range(self.num_threads):
+            arg.append(self.queues[i].queue_to_list())
+        ret = Storage.cache_to_crawl()
+        if ret == 0:  # successful
+            print("To_Crawl links are successfully cached")
+        else:
+            print("Error. Links 'To_crawl' are lost!")
 
     def load_to_crawl(self):
         pass
