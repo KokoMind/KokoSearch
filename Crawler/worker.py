e9b6cd06e69e138078f84bd40b526ddb7f6fd09b
Mostafa Gamal <mgo_oc@hotmail.com>
2017-03-08 23:06:54 +0200

    ....

--- Crawler/worker.py
+++ Crawler/worker.py
@@ -1,21 +1,26 @@
 import threading
 from aptdaemon import lock
-
+from Crawler import frontier
+from Crawler.fetcher import Fetcher
 
 class CrawlerThread(threading.Thread):
     """Class of a thread to crawl the web"""
 
-    def __init__(self, thread_id, name):
+    def __init__(self, thread_id, name,frontier):
         """Construct a new crawling thread"""
         threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.name = name
+        self.frontier = frontier
 
     def run(self):
         """The starting point of a thread"""
-        lock.acquire()
-        #Here call the insertion to frontier queue.
-        lock.release()
+        while True:
+            current_URL = frontier.get_url()
+            code, links, content = Fetcher.fetch(current_URL)
+            if code == -1:
+                continue
+            lock.acquire()
+            frontier.push_to_serve(links)
+            lock.release()
 
-    def log(self):
-        pass
