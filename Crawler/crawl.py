069fd0343c6084ae48451f58c0d9a8047f6d6ac8
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-13 23:35:52 +0200

    Add revisit to main

--- Crawler/crawl.py
+++ Crawler/crawl.py
@@ -1,11 +1,13 @@
 """Main file to start crawling process"""
 
-from Crawler.controller import Controller
+from Crawler.controller import Controller, ControllerRevisit
 from Crawler.fetcher import Fetcher
 
 
 class Config:
     """Class Config contain the configuration of the crawler and the seeds """
+    crawling = True
+    revisiting = False
     num_threads = 20
     cont_to_crawl = False
     seeds = [('https://en.wikipedia.org/', Fetcher.extract_dns('https://en.wikipedia.org/'), (0, 0, 10, 1)),
@@ -17,10 +19,22 @@ class Config:
 
 def crawl():
     # Get the number of Crawler Threads
-    Config.num_threads = int(input("Please Enter the Number of threads:"))
+    Config.num_threads = int(input("Please Enter the Number of threads to crawl:"))
     crawler = Controller(Config.num_threads, Config.seeds, Config.cont_to_crawl)
     crawler.run()
 
 
+def revisit():
+    # Get the number of Crawler Threads
+    Config.num_threads = int(input("Please Enter the Number of threads to revisit:"))
+    crawler = ControllerRevisit(Config.num_threads)
+    crawler.run()
+
+
 if __name__ == "__main__":
-    crawl()
+    if Config.crawling:
+        crawl()
+    elif Config.revisiting:
+        revisit()
+    else:
+        print("Please Choose a proper mode.")
