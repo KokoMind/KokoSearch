d2a535de9753a6d98ea1f877ce43ca8f6f52e7b8
3ammor <o.3ammor@gmail.com>
2017-03-19 14:04:17 +0200

    im memory database

--- Indexer/db_model.py
+++ Indexer/db_model.py
@@ -1,9 +1,11 @@
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 
-db = SqliteExtDatabase('Indexer_cache.db')
+db = SqliteExtDatabase(':memory:')
+db_disk = SqliteExtDatabase('./Indexer_disk.db')
 
 
+# Memory Database
 class BaseModel(Model):
     class Meta:
         database = db
@@ -20,11 +22,6 @@ class Topic(BaseModel):
 
 class Document(BaseModel):
     url = CharField(unique=True)
-    c1 = ForeignKeyField(Topic, related_name='c1')
-    c2 = ForeignKeyField(Topic, related_name='c2')
-    c3 = ForeignKeyField(Topic, related_name='c3')
-    c4 = ForeignKeyField(Topic, related_name='c4')
-    c5 = ForeignKeyField(Topic, related_name='c5')
 
 
 class Word_Doc(BaseModel):
@@ -34,6 +31,39 @@ class Word_Doc(BaseModel):
     neighbours = CharField()
 
 
+# Disk Database
+class BaseModelDisk(Model):
+    class Meta:
+        database = db_disk
+
+
+class WordDisk(BaseModelDisk):
+    word = CharField(unique=True)
+    num_of_docs = IntegerField()
+
+
+class TopicDisk(BaseModelDisk):
+    num_of_docs = IntegerField()
+
+
+class DocumentDisk(BaseModelDisk):
+    url = CharField(unique=True)
+    c1 = ForeignKeyField(TopicDisk, related_name='c1')
+    c2 = ForeignKeyField(TopicDisk, related_name='c2')
+    c3 = ForeignKeyField(TopicDisk, related_name='c3')
+    c4 = ForeignKeyField(TopicDisk, related_name='c4')
+    c5 = ForeignKeyField(TopicDisk, related_name='c5')
+
+
+class Word_DocDisk(BaseModelDisk):
+    doc_id = ForeignKeyField(DocumentDisk, related_name='doc_id')
+    word_id = ForeignKeyField(WordDisk, related_name='word_id')
+    pos = IntegerField()
+    neighbours = CharField()
+
+
 def create_data_base():
     db.connect()
     db.create_tables([Document, Word_Doc, Word, Topic], safe=True)
+    db_disk.connect()
+    db_disk.create_tables([DocumentDisk, Word_DocDisk, WordDisk, TopicDisk], safe=True)
