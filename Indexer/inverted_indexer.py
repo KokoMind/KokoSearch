dffdb27b20f427d326752939adbfcef6f1a6952d
3ammor <o.3ammor@gmail.com>
2017-03-21 17:13:53 +0200

    inverted indexer in mongodb

--- Indexer/inverted_indexer.py
+++ Indexer/inverted_indexer.py
@@ -40,6 +40,7 @@ class InvertedIndexer(Indexer):
         self.flushed_docs = 0
         self.flushed_word_doc = 0
         self.flushed_words = 0
+        self.inverted_collection = db['inverted_indexer']
 
     def _get_word(self, word):
         """get word from the inverted indexer database"""
@@ -61,77 +62,71 @@ class InvertedIndexer(Indexer):
         """add connection between document and word"""
         return Word_Doc.create(doc_id=doc_id, word_id=word_id, pos=pos, neighbours=neighbours)
 
+    def _insert_record(self, word, url, pos, neighbours):
+        record = {"word": word,
+                  "url": url,
+                  "pos": pos,
+                  "neighbours": neighbours}
+        self.inverted_collection.insert_one(record)
+
     def index(self):
         """fill the inverted indexer database"""
         page = self._get_next_page()
         x = 0
         while page != -1:
             x += 1
-            # print(x)
+            print(x)
 
-            if x % 501 == 0:
-                self.flush_to_disk()
             # process over doc
-            page_text = page.content
-            page_text = page_text.lower()
+            page_text = page.content.lower()
             page_url = page.url
-
-            print(len(page_text.split()))
-
-            with db.atomic():
-                doc_tuple = self._get_Document(page_url)
-                if doc_tuple == -1:
-                    doc_tuple = self._add_document(page_url)
 
             tokens = self._tokenizer.tokenize(page_text)
             for i, token in enumerate(tokens):
-                if len(token) < 3 or self._detector.is_stop_word(token):
+                if self._detector.is_stop_word(token):
                     continue
-                else:
-                    stemmed = self._stemmer.stem(token)
-                    word_tuple = self._get_word(stemmed)
-                    if word_tuple == -1:
-                        word_tuple = self._add_word(stemmed)
 
-                    word_tuple.num_of_docs += 1
-                    word_tuple.save()
+                stemmed = self._stemmer.stem(token)
 
-                    neighbours = ''
-                    for j in range(max(0, i - 5), min(len(tokens), i + 5)):
-                        neighbours += (tokens[j] + ' ')
+                if len(stemmed) < 3:
+                    continue
 
-                    self._add_connection(word_tuple.id, doc_tuple.id, i, neighbours)
+                neighbours = ''
+                for j in range(max(0, i - 5), min(len(tokens), i + 5)):
+                    neighbours += (tokens[j] + ' ')
+
+                self._insert_record(stemmed, page_url, i, neighbours)
 
             page = self._get_next_page()
 
-    def flush_to_disk(self):
-        print('documents: ', Document.select().count())
-        print('words', Word.select().count())
-        print('word_docs', Word_Doc.select().count())
+        def flush_to_disk(self):
+            print('documents: ', Document.select().count())
+            print('words', Word.select().count())
+            print('word_docs', Word_Doc.select().count())
 
-        docs_data = []
-        for doc in Document.select().where(Document.id > self.flushed_docs):
-            docs_data.append({'url': doc.url, 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0, 'c5': 0})
+            docs_data = []
+            for doc in Document.select().where(Document.id > self.flushed_docs):
+                docs_data.append({'url': doc.url, 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0, 'c5': 0})
 
-        with db.atomic():
-            DocumentDisk.insert_many(docs_data[:100]).execute()
+            with db.atomic():
+                DocumentDisk.insert_many(docs_data[:100]).execute()
 
-        self.flushed_docs = Document.select().count()
+            self.flushed_docs = Document.select().count()
 
-        word_data = []
-        for word in Word.select().where(Word.id > self.flushed_words):
-            word_data.append({'word': word.word, 'num_of_docs': 0})
+            word_data = []
+            for word in Word.select().where(Word.id > self.flushed_words):
+                word_data.append({'word': word.word, 'num_of_docs': 0})
 
-        with db.atomic():
-            WordDisk.insert_many(word_data[:100]).execute()
+            with db.atomic():
+                WordDisk.insert_many(word_data[:100]).execute()
 
-        self.flushed_words = Word.select().count()
+            self.flushed_words = Word.select().count()
 
-        word_doc_data = []
-        for word_doc in Word_Doc.select().where(Word_Doc.id > self.flushed_word_doc):
-            word_doc_data.append({'doc_id': word_doc.doc_id, 'word_id': word_doc.word_id, 'pos': word_doc.pos, 'neighbours': word_doc.neighbours})
+            word_doc_data = []
+            for word_doc in Word_Doc.select().where(Word_Doc.id > self.flushed_word_doc):
+                word_doc_data.append({'doc_id': word_doc.doc_id, 'word_id': word_doc.word_id, 'pos': word_doc.pos, 'neighbours': word_doc.neighbours})
 
-        with db.atomic():
-            Word_DocDisk.insert_many(word_doc_data[:100]).execute()
+            with db.atomic():
+                Word_DocDisk.insert_many(word_doc_data[:100]).execute()
 
-        self.flushed_word_doc = Word_Doc.select().count()
+            self.flushed_word_doc = Word_Doc.select().count()
