cf45369fc625b3059840c37542b05906966446f1
gemy18 <mahmoud.gemy18@gmail.com>
2017-03-21 17:49:49 +0200

    mongo_db in lda indexer

--- Indexer/lda_indexer.py
+++ Indexer/lda_indexer.py
@@ -12,15 +12,10 @@ class Lda_Indexer(Indexer):
         self._corpus_file=corpus_file
         self._construct_corpus()
         if load != None:
-            gensim.models.ldamodel.LdaModel.load(load)
+            self._load_model()
         if train:
             self._train_model()
-
-
-    def fill_topics(self):
-        #to be continued (add num of docs)
-        for i in range(self.num_topics):
-            Topic.create()
+        self.lda_collection = self.db['lda_indexer']
 
 
     def _construct_corpus(self):
@@ -28,14 +23,14 @@ class Lda_Indexer(Indexer):
         self._corpus = gensim.corpora.MmCorpus(self._corpus_file)
 
     def _train_model(self):
-        print("Start_Training")
         self._lda = LdaMulticore(corpus=self._corpus, id2word=self._id2word, num_topics=self.num_topics, workers=1, chunksize=10000, passes=1)
-        print("Training Done")
-        print("Start Saving")
         self._save_model()
-        print("Saving Done")
     def _save_model(self):
         self._lda.save('lda.model')
+
+    def _load_model(self):
+        self._lda=gensim.models.LdaModel.load('lda.model')
+
 
     def _eval_doc(self,doc):
         doc=doc.split()
@@ -47,11 +42,10 @@ class Lda_Indexer(Indexer):
 
 
     def _add_document(self, url,c1,c2,c3,c4,c5):
-        return Document.create(url=url, c1=c1, c2=c2, c3=c3, c4=c4, c5=c5)
+        new_record={"url":url,"c1":c1,"c2":c2,"c3":c3,"c4":c4,"c5":c5}
+        self.lda_collection.insert_one(new_record)
 
-    def _update_document(self, doc,c1,c2,c3,c4,c5):
-        doc.c1,doc.c2,doc.c3,doc.c4,doc.c5=c1,c2,c3,c4,c5
-        doc.save()
+
 
     def index(self):
         page = self._get_next_page()
@@ -59,11 +53,8 @@ class Lda_Indexer(Indexer):
             page_text = page.content
             page_url = page.url
             top_topics=self._eval_doc(page_text)
-            with db.atomic():
-                doc_tuple = self._get_Document(page_url)
-                if doc_tuple == -1:
-                    doc_tuple = self._add_document(page_url,top_topics[0],top_topics[1],top_topics[2],top_topics[3],top_topics[4])
-                else :
-                    self._update_document(top_topics[0],top_topics[1],top_topics[2],top_topics[3],top_topics[4])
-            page = self._get_next_page()
+            doc_tuple = self._get_Document(page_url)
+            self._add_document(page_url,top_topics[0],top_topics[1],top_topics[2],top_topics[3],top_topics[4])
+
+        page = self._get_next_page()
 
