4c02a57fb23381b54e7aa641c9e1d6d826f2d3cd
gemy18 <mahmoud.gemy18@gmail.com>
2017-03-25 17:01:42 +0200

    fix testing

--- Indexer/lda_indexer_test.py
+++ Indexer/lda_indexer_test.py
@@ -2,9 +2,10 @@ from Indexer.utils import *
 from gensim import corpora, models
 from gensim.models.ldamulticore import  LdaMulticore
 import operator
-
-id2word = corpora.Dictionary.load_from_text('/home/gemy/work/MachineLearning/my-projects/KokoSearch/Indexer/lda_model/trained_lda.model.id2word')
-lda=models.LdaModel.load('/home/gemy/work/MachineLearning/my-projects/KokoSearch/Indexer/lda_model/trained_lda.model')
+path=""
+path2=""
+id2word = corpora.Dictionary.load_from_text(path)
+lda=models.LdaModel.load(path2)
 new_doc=id2word.doc2bow("i'm playing football")
 sorted_probs=lda[new_doc].sort(key=operator.itemgetter(1))
 top_topics, _ = [list(c) for c in zip(*sorted_probs[:5])]
