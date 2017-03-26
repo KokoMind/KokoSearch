5976be331f94e5e1f01d74cbfbcff2a334dc3646
3ammor <o.3ammor@gmail.com>
2017-03-26 17:01:48 +0200

    indexer

--- Indexer/inverted_indexer_test.py
+++ Indexer/inverted_indexer_test.py
@@ -1,7 +1,7 @@
 from Indexer.inverted_indexer import *
 import time
 
-threads_num = 8
+threads_num = 32
 
 
 def test():
--- Indexer/prepare_indexer_data.py
+++ Indexer/prepare_indexer_data.py
@@ -5,11 +5,19 @@ import pymongo
 def prepare():
     crawler_db = MongoClient()['crawled']
     crawled = crawler_db['crawled']
-    crawled.create_index([('my_id', pymongo.ASCENDING)])
+    result = crawled.create_index([('page_id', pymongo.ASCENDING)], unique=True)
 
-    print(sorted(list(crawled.index_information())))
 
-    for i, s in enumerate(crawled.find()):
-        crawled.update_one({'_id': s['_id']}, {'$inc': {'my_id_1': i}})
-        if i % 10000 == 0:
-            print(i)
+test_db = MongoClient()['test']
+test = test_db['test']
+
+test.create_index([('my_id', pymongo.ASCENDING)])
+
+print(sorted(list(test.index_information())))
+
+for i, s in enumerate(test.find()):
+    test.update_one({'_id': s['_id']}, {'$inc': {'my_id_1': i}})
+    if i % 10000 == 0:
+        print(i)
+
+print(test.find({'my_id_1': 0})[0])
