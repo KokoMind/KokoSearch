921e89e92cd69ae142f4ff3cf64442ee0288af51
3ammor <o.3ammor@gmail.com>
2017-03-28 14:34:19 +0200

    small fix

--- Indexer/prepare_indexer_data.py
+++ Indexer/prepare_indexer_data.py
@@ -5,7 +5,7 @@ import pymongo
 def prepare():
     crawler_db = MongoClient()['crawled']
     crawled = crawler_db['crawled']
-    crawled.create_index([('page_id', pymongo.ASCENDING)], unique=True)
+    crawled.create_index([('my_id', pymongo.ASCENDING)])
 
     for i, s in enumerate(crawled.find()):
         crawled.update_one({'_id': s['_id']}, {'$set': {'my_id_1': i}})
