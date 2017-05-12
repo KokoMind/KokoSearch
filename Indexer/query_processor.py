e843850f480efdbde80d680f1dfe33f4a5f27a03
gemy18 <mahmoud.gemy18@gmail.com>
2017-05-12 14:30:18 +0200

    query processor

--- Indexer/query_processor.py
+++ Indexer/query_processor.py
@@ -1,12 +1,21 @@
 from utils import *
-
+import gensim
 
 class QueryProcessor:
-    def __init__(self):
+    def __init__(self, _id2word_path, corpus_path,model_path):
         self._stemmer = Stemmer()
         self._tokenizer = Tokenizer()
         self._detector = StopWordsDetector()
 
+        self._id2word_path = _id2word_path
+        self._corpus_path = corpus_path
+        self._construct_corpus()
+        self.model_path=model_path
+    def _construct_corpus(self):
+        self._id2word = gensim.corpora.Dictionary.load_from_text(self._id2word_path)
+        self._corpus = gensim.corpora.MmCorpus(self._corpus_path)
+    def _load_model(self, load):
+        self._lda = gensim.models.LdaModel.load(self.model_path)
     def is_qoute(self, query):
         return query[0] == "\"" and query[-1] == "\""
 
@@ -20,3 +29,10 @@ class QueryProcessor:
                     stemmed.append(stemmed_token)
 
         return stemmed
+    def get_topic(self,query):
+        query = query.split()
+        new_query = self._id2word.doc2bow(query)
+        sorted_probs = sorted(self._lda[new_query], key=lambda x: x[1])
+        top_topics = [key for key, _ in sorted_probs[:2]]
+        top_topics += [-1] * 5
+        return top_topics[0]
