de9c8258ea03ea0d3db1305b41597f78e9da465c
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-05-12 15:41:30 +0200

    initial django app

--- kokosearch/urls.py
+++ kokosearch/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from search import views as search_views
 
 urlpatterns = [
+    url(r'^search/', include('search.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', search_views.index),
 ]
