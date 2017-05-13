8d16b7f7cf8fc2135ae06ed42370a5177a55989f
gemy18 <mahmoud.gemy18@gmail.com>
2017-05-13 03:13:02 +0200

    ranker

--- Ranker/test.py
+++ Ranker/test.py
@@ -5,4 +5,5 @@ corpus = "./results/results_tfidf.mm"
 model = "./lda_model/lda.model"
 rank = Ranker(_id2word_path=id2word_file, corpus_path=corpus, model_path=model)
 
-rank.search("dogs")
+lis,_=rank.search("dogs")
+print (lis[:20])
