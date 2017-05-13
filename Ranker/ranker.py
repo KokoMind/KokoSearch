51d5a3c1a11e9cf3fd4a800c444899f7730f4ed3
3ammor <o.3ammor@gmail.com>
2017-05-13 19:19:56 +0200

    format

--- Ranker/ranker.py
+++ Ranker/ranker.py
@@ -43,10 +43,9 @@ class Ranker:
                             snapits[record['url']] = record['neighbours']
                 batch.close()
 
-        if not self.query_processor.is_qoute(query):
-            results = sorted(score.items(), key=operator.itemgetter(1), reverse=True)
+        results = sorted(score.items(), key=operator.itemgetter(1), reverse=True)
 
-            urls = [(url, snapits[url][:350]) for (url, value) in results][:int(self.inverted_indexer_results_num / 16)]
+        urls = [(url, snapits[url][:350]) for (url, value) in results][:int(self.inverted_indexer_results_num / 16)]
 
         return urls
 
