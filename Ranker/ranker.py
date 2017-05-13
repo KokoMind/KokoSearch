0798bb4053ea6f5b382941fc813d936cf9310d45
3ammor <o.3ammor@gmail.com>
2017-05-13 02:33:12 +0200

    format

--- Ranker/ranker.py
+++ Ranker/ranker.py
@@ -1,14 +1,15 @@
-import  gensim
+import gensim
 from query_processor import QueryProcessor
 from pymongo import MongoClient
 import operator
 
+
 class Ranker:
     def __init__(self, _id2word_path, corpus_path, model_path):
         self.query_processor = QueryProcessor(_id2word_path, corpus_path, model_path)
-        self.inverted_indexer_results_num = 50
+        self.inverted_indexer_results_num = 1000
         self.inverted_collections = []
-        self.lda_collections=[]
+        self.lda_collections = []
         for i in range(16):
             db = MongoClient()['inverted_database_final_{0}'.format(i)]
             self.inverted_collections.append(db['inverted_indexer'])
@@ -16,8 +17,6 @@ class Ranker:
         for i in range(4):
             db = MongoClient()['lda_database_final_{0}'.format(i)]
             self.lda_collections.append(db['lda_indexer'])
-
-
 
     def inverted_indexer_search(self, query):
         urls = []
@@ -39,29 +38,30 @@ class Ranker:
                     batch = self.inverted_collections[i].find({'word': token})
                     for record in batch:
                         score[record['url']] += 1
-                        snapits[record['url']] += record['neighbours']
+                        snapits[record['url']] += ' - ' + record['neighbours']
 
                 results = sorted(score.items(), key=operator.itemgetter(1))
 
                 urls += [url for url, value in results.items()][:self.inverted_indexer_results_num / 16]
 
         return urls, snapits
-    def search(self,query):
-        query_topic=self.query_processor.get_topic(query)
-        inverted_indexer_urls,snippets=self.inverted_indexer_search(query)
-        same_topic={}
+
+    def search(self, query):
+        query_topic = self.query_processor.get_topic(query)
+        inverted_indexer_urls, snippets = self.inverted_indexer_search(query)
+        same_topic = {}
         for url in inverted_indexer_urls:
-            same_topic[url]=False
+            same_topic[url] = False
         for i in range(4):
-            batch = self.lda_collections[i].find({"c1":query_topic,'url': {'$in': inverted_indexer_urls}}, no_cursor_timeout=True)
+            batch = self.lda_collections[i].find({"c1": query_topic, 'url': {'$in': inverted_indexer_urls}}, no_cursor_timeout=True)
             for record in batch:
-                same_topic[record['url']]=True
-        lis1=[]
-        lis2=[]
+                same_topic[record['url']] = True
+        lis1 = []
+        lis2 = []
         for url in inverted_indexer_urls:
             if same_topic[url]:
                 lis1.append(url)
-            else :
+            else:
                 lis2.append(url)
 
-        return lis1+lis2,snippets
+        return lis1 + lis2, snippets
